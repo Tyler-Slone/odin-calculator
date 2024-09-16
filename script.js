36bddb24337e40eed20616546d120589c83c8a40
Tyler Slone <tyl3rslon3@gmail.com>
2024-09-16 17:46:09 -0400

    Add document selectors

---
+++ script.js
@@ -0,0 +1,18 @@
+let current = ''
+let previsous = ''
+
+const numberButtons = document.querySelectorAll('numbers')
+
+const divisionButton = document.getElementById('divide')
+const multiplicationButton = document.getElementById('multiply')
+const subtractionButton = document.getElementById('subtract')
+const addButton = document.getElementById('add')
+const calculateButton = document.getElementById('calculate')
+const positiveNegative = document.getElementById('positiveNegative')
+
+const clearButton= document.getElementById('clear')
+const deleteButton = document.getElementById('delete')
+
+const previousDisplay = document.getElementById('previous')
+const currentDisplay = document.getElementById('current')
+
